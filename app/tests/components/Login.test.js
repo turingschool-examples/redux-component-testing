7eec65e497061327b4079081e3892c8c90f911c9
Brittany Storoz <brittanystoroz@gmail.com>
2017-03-29 11:09:50 -0600

    Brenna

--- app/tests/components/Login.test.js
+++ app/tests/components/Login.test.js
@@ -1,1 +1,83 @@
-// Unit test the unwrapped Login component
+import React from 'react';
+import { shallow } from 'enzyme';
+import fetchMock from 'fetch-mock';
+import { browserHistory } from 'react-router';
+
+import Login from '../../components/Login';
+
+describe('Login Component', () => {
+  const mockUser = {
+    data: {
+      name: 'Bob LobLaw',
+      id: 1,
+      email: 'foo@bar.com'
+    }
+  }
+
+  const LoginComponent = shallow(
+    <Login user={mockUser.data} signIn={jest.fn()} />
+  );
+
+  afterEach(() => {
+    expect(fetchMock.calls().unmatched).toEqual([]);
+    fetchMock.restore();
+  });
+
+
+  it('should display error when authentication fails', async (done) => {
+    fetchMock.post('http://localhost:3001/authenticate', { status: 500, body: {} });
+
+    let emailInput = LoginComponent.find('input[name="email"]');
+    let submitButton = LoginComponent.find('button');
+
+    emailInput.simulate('change', {
+      target: {
+        name: 'email',
+        value: 'foo@bar.com'
+      }
+    });
+
+    submitButton.simulate('click');
+
+    await LoginComponent.update();
+
+    let expectedErrorMessage = 'Invalid Credentials';
+    let errorElement = LoginComponent.find('.errorMessage');
+
+    expect(LoginComponent.state().error).toEqual(expectedErrorMessage);
+    expect(errorElement.length).toEqual(1);
+    expect(errorElement.text()).toEqual(expectedErrorMessage);
+
+    done();
+  });
+
+  it('redirects to dashboard on successful login', async (done) => {
+    spyOn(browserHistory, 'push');
+
+    fetchMock.post('http://localhost:3001/authenticate', {
+      status: 200, 
+      ok: true,
+      body: mockUser
+    });
+
+    let emailInput = LoginComponent.find('input[name="email"]');
+    let submitButton = LoginComponent.find('button');
+
+    emailInput.simulate('change', {
+      target: {
+        name: 'email',
+        value: 'foo@bar.com'
+      }
+    });
+
+    submitButton.simulate('click');
+
+    await LoginComponent.update();
+
+    expect(browserHistory.push).toHaveBeenCalledWith('/dashboard');
+
+    done();
+
+  })
+
+});
--- app/tests/containers/LoginContainer.test.js
+++ app/tests/containers/LoginContainer.test.js
@@ -1,1 +1,48 @@
-// Test the connected LoginContainer component
+import React from 'react';
+import { mount } from 'enzyme';
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+
+import LoginContainer from '../../containers/LoginContainer';
+import Login from '../../components/Login';
+
+const mockStore = configureMockStore()({
+  user: {
+    name: 'Bob Loblaw',
+    id: 1,
+    email: 'foo@bar.com'
+  }
+});
+
+const setup = () => {
+  const Container = mount(
+    <Provider store={mockStore}>
+      <LoginContainer />
+    </Provider>
+  );
+
+  const Component = Container.find(Login);
+
+  return {
+    Container,
+    Component
+  }
+}
+
+describe('LoginContainer', () => {
+  const { Container, Component } = setup();
+
+  it('should pass the appropriate props from state', () => {
+    expect(Component.props().user).toEqual({
+      name: 'Bob Loblaw',
+      id: 1,
+      email: 'foo@bar.com'
+    });
+  });
+
+  it('should pass down the correct action creators as props', () => {
+    expect(Object.keys(Component.props())).toContain(
+      'signIn', 'signOut', 'signInFailed'
+    );
+  });
+});
